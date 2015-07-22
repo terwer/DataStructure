caeff3c461f66bdb432e760f4c3048644d8b22bd
terwer <cbgtyw@gmail.com>
2015-07-22 13:28:39 +0800

    Update project

---
+++ sort/InsertionSort/InsertionSort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  InsertionSort
+//
+//  Created by Terwer Green on 15/7/22.
+//  Copyright (c) 2015年 Terwer Green. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
